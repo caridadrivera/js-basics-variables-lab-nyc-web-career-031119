337c7bac1ce83d546a6a331a1e9c32fb092b85f2
Caridad Rivera <ccrive10@gmail.com>
2019-04-20 13:10:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+
+
+// function companyName() {
+//  const companyName = 'Scuber';
+// }
+
+let companyName = 'Scuber';
+let companyCeo = 'Susan Smith';
+let mostProfitableNeighborhood  = 'Chelsea';
